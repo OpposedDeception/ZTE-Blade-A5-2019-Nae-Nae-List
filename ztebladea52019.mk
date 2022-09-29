f145883e7b721bf0302b79375f2a1de58ebea8cb
KotenAngered <113610915+KotenAngered@users.noreply.github.com>
2022-09-29 10:14:59 +0200

    Add files via upload

---
+++ ztebladea52019.mk
@@ -0,0 +1,20 @@
+# Architecture 
+ TARGET_ARCH := armv7-a-neon 
+ TARGET_ARCH_VARIANT := armeabi-v7a
+ TARGET_CPU_ABI := armv8.2-A
+ TARGET_CPU_ABI2 := aarch64
+ TARGET_CPU_VARIANT := generic 
+  
+ TARGET_2ND_ARCH := arm 
+ TARGET_2ND_ARCH_VARIANT := armv7-a-neon 
+ TARGET_2ND_CPU_ABI := armeabi-v7a 
+ TARGET_2ND_CPU_ABI2 := arm
+ TARGET_2ND_CPU_VARIANT := generic 
+ TARGET_BOARD_SUFFIX := _32
+ TARGET_USES_64_BIT_BINDER := false
+  
+ # Assert 
+ TARGET_OTA_ASSERT_DEVICE := zte
+  
+ # Bootloader 
+ TARGET_BOOTLOADER_BOARD_NAME := ztebladea52019
